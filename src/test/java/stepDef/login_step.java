5bf50aff08ce25a582e9f85e59032a70e71ef14e
Bakhtiyar2021 <bakhtiyaruddin@yahoo.com>
2021-10-26 13:03:40 -0400

    honda

---
+++ src/test/java/stepDef/login_step.java
@@ -0,0 +1,7 @@
+package stepDef;
+
+public class login_step {
+    @io.cucumber.java.en.Given("^I am at TalentTEK Homepage$")
+    public void iAmAtTalentTEKHomepage() {
+    }
+}
--- src/test/java/testRuuner.java
+++ src/test/java/testRuuner.java
@@ -1,3 +1,5 @@
+@Cu
+
 
 
 public class testRuuner {
